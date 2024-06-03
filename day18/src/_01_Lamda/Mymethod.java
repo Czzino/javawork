24ec751353400f7c5edbe6ed739fc0a48188c89a
Czzino <fvwlharu@naver.com>
2024-06-03 18:07:56 +0900

    day18

--- day18/src/_01_Lamda/Mymethod.java
+++ day18/src/_01_Lamda/Mymethod.java
@@ -1,10 +1,8 @@
 package _01_Lamda;
 
 // 함수식 interface를 체크하는 어노테이션
-// 추상메서드가 1개인지를 체크
+//  추상메서드가 1개인지를 체크
 @FunctionalInterface
-public interface Mymethod {
-	// 추상메서드 구현 생략 가능 public abstract
-	// 매개변수가 하나도 없음
-	int method();
+public interface MyMethod {
+	/* public abstract */ void method();
 }
--- day18/src/_01_Lamda/T01_lamda.java
+++ day18/src/_01_Lamda/T01_lamda.java
@@ -3,26 +3,26 @@ package _01_Lamda;
 public class T01_lamda {
 
 	public static void main(String[] args) {
-
 		// 람다식 사용할 때 사용하는 interface
-		// 1. 추상메서드로 구현
-		// 2. 추상메서드는 반드시 1개만 있어야 됨
-		Mymethod mm1 = new Mymethod() {
+		//   1. 추상메서드로 구현
+		//	 2. 추상메서드는 반드시 1개만 있어야 됨
+		MyMethod mm1 = new MyMethod() {
 			@Override
 			public void method() {
 				System.out.println("method() call");
 			}
-
 		};
 		mm1.method();
-
+		
 		mm1 = () -> {
 			System.out.println("람다에서 출력");
 		};
 		mm1.method();
-
-		// 한줄일때는 생략가능
+		
 		mm1 = () -> System.out.println("람다2에서 출력");
 		mm1.method();
 	}
 }
+
+
+
